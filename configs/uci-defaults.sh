21c509b262c88366cab1c4881f3560ef5959c870
ganxy <ganxy1028@outlook.com>
2026-09-12 11:11:39 +0800

    直接配置uci-default

--- configs/uci-defaults.sh
+++ configs/uci-defaults.sh
@@ -1,31 +1,13 @@
 #!/bin/sh
-# 99-custom.sh 就是immortalwrt固件首次启动时运行的脚本 位于固件内的/etc/uci-defaults/99-custom.sh
+# 99-custom 就是immortalwrt固件首次启动时运行的脚本 位于固件内的/etc/uci-defaults/99-custom
 # Log file for debugging
-LOGFILE="/etc/config/uci-defaults/uci-defaults-log"
-echo "Starting 99-custom.sh at $(date)" >>$LOGFILE
-# 因为本项目中 单网口模式是dhcp模式 直接就能上网并且访问web界面 避免新手每次都要修改/etc/config/network中的静态ip
+LOGFILE="/tmp/log/uci-defaults-log"
+echo "Starting 99-custom at $(date)" >>$LOGFILE
 
-# 检查自定义路由器管理地址文件是否存在
-IP_VALUE_FILE="/etc/config/uci-defaults/custom_router_ip"
-if [ -f "$IP_VALUE_FILE" ]; then
-    # 读取自定义路由器管理地址
-    . "$IP_VALUE_FILE"
-    echo "Custom router IP: $custom_router_ip" >>$LOGFILE
-else
-    custom_router_ip="192.168.100.1"
-    echo "Custom router IP file not found. Using default IP: $custom_router_ip" >>$LOGFILE
-fi
-
-# 检查配置文件pppoe-settings是否存在 该文件由build.sh动态生成
-PPPOE_SETTINGS_FILE="/etc/config/uci-defaults/pppoe-settings"
-if [ ! -f "$PPPOE_SETTINGS_FILE" ]; then
-    echo "PPPoE settings file not found. Skipping." >>$LOGFILE
-    enable_pppoe="no"
-else
-    # 读取pppoe信息($enable_pppoe、$pppoe_account、$pppoe_password)
-    . "$PPPOE_SETTINGS_FILE"
-fi
-
+# lan_ip_address=""
+#
+# pppoe_username=""
+# pppoe_password=""
 
 # 1. 先获取所有物理接口列表
 ifnames=""
@@ -65,7 +47,6 @@ elif [ "$count" -gt 1 ]; then
     # 配置WAN
     uci set network.wan=interface
     uci set network.wan.device="$wan_ifname"
-    uci set network.wan.proto='dhcp'
 
     # 查找 br-lan 设备 section
     section=$(uci show network | awk -F '[.=]' '/\.@?device\[\d+\]\.name=.br-lan.$/ {print $2; exit}')
@@ -82,24 +63,29 @@ elif [ "$count" -gt 1 ]; then
     fi
 
     # LAN口设置静态IP
-    uci set network.lan.proto='static'
-    # 多网口设备 支持修改为别的管理后台地址 在Github Action 的UI上自行输入即可 
-    uci set network.lan.netmask='255.255.255.0'
-    # 设置路由器管理后台地址
-    uci set network.lan.ipaddr="$custom_router_ip"
+    if [ -n "$lan_ip_address" ]; then
+        uci set network.lan.proto='static'
+        # 多网口设备 支持修改为别的管理后台地址 在Github Action 的UI上自行输入即可 
+        uci set network.lan.netmask='255.255.255.0'
+        # 设置路由器管理后台地址
+        uci set network.lan.ipaddr="$lan_ip_address"
+    fi
+
+    # 配置WAN口
 
     # PPPoE设置
-    echo "enable_pppoe value: $enable_pppoe" >>$LOGFILE
-    if [ "$enable_pppoe" = "yes" ]; then
-        echo "PPPoE enabled, configuring..." >>$LOGFILE
-        uci set network.wan.proto='pppoe'
-        uci set network.wan.username="$pppoe_account"
+    if [ -n "$pppoe_username" -a "$pppoe_password" ]; then
+        uci set network.wan.proto=pppoe
+        uci set network.wan.username="$pppoe_username"
         uci set network.wan.password="$pppoe_password"
-        uci set network.wan.peerdns='1'
-        uci set network.wan.auto='1'
+        uci delete network.wan6
         echo "PPPoE config done." >>$LOGFILE
     else
-        echo "PPPoE not enabled." >>$LOGFILE
+        uci set network.wan.proto='dhcp'
+        # 配置WAN6
+        uci set network.wan6=interface
+        uci set network.wan6.device="$wan_ifname"
+        uci set network.wan6.proto='dhcpv6'
     fi
 
     uci commit network
